a4f572d232f9da599bd0ef55429c1f392c3a460a
Kundan-Patil7 <kpatil.pale@gmail.com>
2025-04-19 23:43:03 +0530

    setup

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+
+import './App.css'
+import { BrowserRouter, Route, Router } from 'react-router'
+
+function App() {
+
+
+  return (
+    <>
+     
+    <BrowserRouter>
+    <Router>
+      <Route  path='/' element={} />
+    </Router>
+    </BrowserRouter>
+
+    </>
+  )
+}
+
+export default App
